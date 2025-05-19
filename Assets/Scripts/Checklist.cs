bb85ffffa3194dcaf8565d534f879e680b4cee10
Odonata-Masoni <investgramvn@gmail.com>
2025-05-19 19:59:58 +0700

    Them checklist cai thien demo

---
+++ Assets/Scripts/Checklist.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checklist : MonoBehaviour
+{
+    //Camera Movement and lock on Bottom of the Scene
+    // Health bar UI
+    // Enemy move toward player if Is Detected
+    // Design 3 levels with 3 different Environments and Enemy set
+    // Last Lever with Boss Design
+
+   
+}
